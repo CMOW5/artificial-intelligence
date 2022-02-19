d40daf42f00100f677d62ccc3a45fe722ad80138
cmow5 <tec.cmos@gmail.com>
2022-02-19 15:13:29 -0500

    value iteration first attempt

--- berkeley/4-reinforcement-learning/project/valueIterationAgents.py
+++ berkeley/4-reinforcement-learning/project/valueIterationAgents.py
@@ -31,6 +31,7 @@ import mdp, util
 from learningAgents import ValueEstimationAgent
 import collections
 
+
 class ValueIterationAgent(ValueEstimationAgent):
     """
         * Please read learningAgents.py before reading this.*
@@ -40,7 +41,8 @@ class ValueIterationAgent(ValueEstimationAgent):
         for a given number of iterations using the supplied
         discount factor.
     """
-    def __init__(self, mdp, discount = 0.9, iterations = 100):
+
+    def __init__(self, mdp: mdp.MarkovDecisionProcess, discount=0.9, iterations=100):
         """
           Your value iteration agent should take an mdp on
           construction, run the indicated number of iterations
@@ -56,12 +58,107 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.mdp = mdp
         self.discount = discount
         self.iterations = iterations
-        self.values = util.Counter() # A Counter is a dict with default 0
+        self.values = util.Counter()  # A Counter is a dict with default 0
         self.runValueIteration()
 
     def runValueIteration(self):
         # Write value iteration code here
+        print('value iteration')
+        print('MDP = ', self.mdp)
+        print('values = ', self.values)
+
+        print('MDP = ', self.mdp.getStates())
+        print('initial state = ', self.mdp.getStartState())
+        print('possible actions = ', self.mdp.getPossibleActions((0, 0)))
+        print('transition states and probs = ', self.mdp.getTransitionStatesAndProbs((0, 0), 'east'))
+
+        # print('possible actions = ', self.mdp.getPossibleActions())
+
         "*** YOUR CODE HERE ***"
+        # todo:
+        # self.values[(0, self.mdp.getStartState(), which_action)] = 0
+
+        v_values = dict()  # need to select the max of these
+
+        # init V0(s) = 0
+        for s in self.mdp.getStates():
+            v_values[(0, s)] = 0, None
+
+        for k in range(1, self.iterations - 1):
+
+            for s_state in self.mdp.getStates():
+
+                local_v_values = dict()
+
+                for action in self.mdp.getPossibleActions(s_state):
+
+                    transition = self.mdp.getTransitionStatesAndProbs(s_state, action)
+
+                    q_value = 0
+
+                    # Sum s' T(s, a, s') * [R(s, a, s') + Gamma * Vk(s')]
+                    # s = state, a = actions, s' = next_state, T(s, a, s') = probability
+                    for (next_state, probability) in transition:
+                        # R(s, a, s')
+                        reward = self.mdp.getReward(s_state, action, next_state)
+
+                        # Vk(s') todo
+                        v_i_value, v_i_action = v_values[(k - 1, next_state)]
+
+                        q_value += probability * (reward + (self.discount * v_i_value))
+
+                    # v_values[k, state, action] = q_value
+                    local_v_values[(s_state, action)] = q_value
+
+                # get max local_v_value and assign it to v_values
+                print('local_v_values = ', local_v_values)
+
+                if len(local_v_values) > 0:
+                    max_local_v_value = sorted(local_v_values.items(), key=lambda item: item[1], reverse=True)[0]
+                    (s, a), max_value = max_local_v_value
+                    v_values[(k, s)] = (max_value, a)
+                else:
+                    """
+                    Note: Make sure to handle the case when a state has no available actions in an MDP 
+                    (think about what this means for future rewards).
+                    """
+                    v_values[(k, s_state)] = (0, None)
+
+                #self.values[s_state] =
+
+        print('end v iteration')
+        # get the last iteration values
+        for (k, state) in v_values:
+            if k is (self.iterations - 2):
+                self.values[state] = v_values[(k, state)]
+
+        print('self_values = ', self.values)
+        """
+        for k in range(1, self.iterations - 1):
+
+            actions = self.mdp.getPossibleActions(state)
+
+            local_v_values = dict()
+
+            for action in actions:
+                transition = self.mdp.getTransitionStatesAndProbs(state, action)
+                q_value = 0
+
+                # Sum s' T(s, a, s') * [R(s, a, s') + Gamma * Vk(s')]
+                # s = state, a = actions, s' = next_state, T(s, a, s') = probability
+                for (next_state, probability) in transition:
+                    # R(s, a, s')
+                    reward = self.mdp.getReward(state, action, next_state)
+
+                    # Vk(s') todo
+                    v_i_value = v_values[(k - 1, next_state)]
+
+                    q_value += probability * (reward + (self.discount * v_i_value))
+
+                # v_values[k, state, action] = q_value
+                local_v_values[(action,)] = q_value
+        """
+
 
     def getValue(self, state):
         """
@@ -73,10 +170,13 @@ class ValueIterationAgent(ValueEstimationAgent):
         """
           Compute the Q-value of action in state from the
           value function stored in self.values.
+
+          returns the Q-value of the (state, action) pair given by the value function given by self.values
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #self.values[state]
 
+        util.raiseNotDefined()
     def computeActionFromValues(self, state):
         """
           The policy is the best action in the given state
@@ -85,8 +185,13 @@ class ValueIterationAgent(ValueEstimationAgent):
           You may break ties any way you see fit.  Note that if
           there are no legal actions, which is the case at the
           terminal state, you should return None.
+
+          computes the best action according to the value function given by self.values.
         """
         "*** YOUR CODE HERE ***"
+        print('state = ', state)
+        # something with ??
+        #self.values[state]
         util.raiseNotDefined()
 
     def getPolicy(self, state):
